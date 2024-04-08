68af02291af00dcd5bb10851fa0acfe33c090146
ChetanAutomationWorld <chetanmetker@gmail.com>
2024-04-08 13:22:59 +0530

    help text added

--- src/ArrayPractice/ArrayBetweenTwoNumbers.java
+++ src/ArrayPractice/ArrayBetweenTwoNumbers.java
@@ -7,6 +7,7 @@ public class ArrayBetweenTwoNumbers {
     public static void main(String[] args) {
 
         // 1-->4 : 1,2,3,4
+        // 1--> 10: 1,2,3,4...9,10
 
         System.out.println(Arrays.toString(between(1,4)));
 
