3cc4b9f84efde63a63df5834a2dd8d5a87c041a6
ChetanAutomationWorld <chetanmetker@gmail.com>
2024-04-08 13:16:52 +0530

    added array file

---
+++ src/ArrayPractice/ArrayBetweenTwoNumbers.java
@@ -0,0 +1,24 @@
+package ArrayPractice;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class ArrayBetweenTwoNumbers {
+    public static void main(String[] args) {
+
+        // 1-->4 : 1,2,3,4
+
+        System.out.println(Arrays.toString(between(1,4)));
+
+
+    }
+
+
+    public static int[] between(int a, int b){
+        return IntStream.rangeClosed(a,b).toArray();
+    }
+
+
+}
+
+
