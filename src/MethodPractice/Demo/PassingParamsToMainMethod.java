bf6e8849731685e827795515aeaac1d89d4b5bb6
ChetanAutomationWorld <chetanmetker@gmail.com>
2024-04-15 21:46:35 +0530

    Passing Parameter into main method

---
+++ src/MethodPractice/Demo/PassingParamsToMainMethod.java
@@ -0,0 +1,14 @@
+package MethodPractice.Demo;
+
+public class PassingParamsToMainMethod {
+
+    public static void main(String args[]) {
+
+        System.out.println(args.length);
+
+        for (String value:args){
+            System.out.println(value);
+        }
+
+    }
+}
